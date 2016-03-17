2a71c76cf847fa8fad90f78d5982efba1703c44a
John Stoddard <john.stoddard@oracle.com>
2016-03-16 17:56:14 -0700

    Get rid of dead code. More cleanup.

--- nc-z.py
+++ nc-z.py
@@ -2,26 +2,27 @@
 import sys, re, socket
 from contextlib import closing
 SCRIPT_NAME = sys.argv[0]
-
 # jstoddar at g mail .com
-# python script/lib to replace missing -z parameter to nc.
-#  seems to have gone away in the nc in rev7 rh and clones.
+'''nc-z.py: script to check to see if port is open.
+Handy to have where you can't install nc or nmap.
+Output and status largely mimic "nc -z". '''
+
+# The -z option to nc has annoyingly turned up missing in rhel7 nc
+#  and clone versions.
 # Output intended to "fool" bash scripts that formerly used
 #  nc -z [-v] [-w nseconds] <host> <port>
-# If you like you can wrapper nc and use this when "-z" prameter
-#  is passed to nc. This ugliness is up to you.
+# If you like, you can wrapper nc and use this script when
+#  the "-z" prameter is passed to nc. This ugliness is up to you.
 
 def usage():
     ''' Usage function '''
-    print "Usage: " + SCRIPT_NAME + " [-v|--verbose] [-w|-t|--timeout nseconds] \
- <hostname> <port>"
+    print "Usage: " + SCRIPT_NAME + \
+    " [-v|--verbose] [-w|-t|--timeout nseconds] <hostname> <port>"
     print "example: " + SCRIPT_NAME + " -v localhost 222 -w 2.0"
     sys.exit(2)
 
 def parse_input():
-    '''
-    Parse input, getopts and argparse didn't seem to be worthwhile to deal with.
-    '''
+    ''' Parse input args. '''
     v = False
     t = 1.0 # default timeout, note nc defaults to no timeout.
     args = []
@@ -39,6 +40,7 @@ def parse_input():
 
 
 def get_port_service(port):
+    ''' Get service name for port out of /etc/services. '''
     with open("/etc/services") as f:
         for line in f:
             search_regex = r"(^[a-z]+)(\s+)" + re.escape(port) + r"(\s+)"
@@ -58,16 +60,12 @@ def ck_socket(host, port, timeout, verbose):
                 service = get_port_service(port + "/tcp")
                 print "Connection to " + host + " " + port \
                 + " port [tcp/" + service + "] succeeded!"
-                sys.exit(0)
-            else:
-                sys.exit(0)
+            sys.exit(0)
         else:
             if verbose:
-                print SCRIPT_NAME + ": connect to " + host + " port " + port \
-                + " (tcp) failed: "
-                sys.exit(1)
-            else:
-                sys.exit(1)
+                print SCRIPT_NAME + ": connect to " + host \
+                + " port " + port + " (tcp) failed: "
+            sys.exit(1)
 
 def main():
     verbose, timeout, params = parse_input()

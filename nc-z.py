7bc6e837db50e30c4ebdcd1a1e7282a972cccd52
John Stoddard <jstoddar@gmail.com>
2016-03-17 19:22:26 -0700

    Fix regex function regex and returned defaults. Move exits out of ck_socket function and into main. ck_socket may now be elsewhere if desired.

--- nc-z.py
+++ nc-z.py
@@ -3,10 +3,11 @@ import sys, re, socket
 from contextlib import closing
 SCRIPT_NAME = sys.argv[0]
 # jstoddar at g mail .com
-'''nc-z.py: script to check to see if port is open.
+'''
+nc-z.py: script to check to see if port is open.
 Handy to have where you can't install nc or nmap.
-Output and status largely mimic "nc -z". '''
-
+Output and status largely mimic "nc -z".
+'''
 # The -z option to nc has annoyingly turned up missing in rhel7 nc
 #  and clone versions.
 # Output intended to "fool" bash scripts that formerly used
@@ -38,42 +39,48 @@ def parse_input():
             args.append(arg)
     return v, t, args
 
-
 def get_port_service(port):
     ''' Get service name for port out of /etc/services. '''
+    regex_out = '*'
     with open("/etc/services") as f:
         for line in f:
-            search_regex = r"(^[a-z]+)(\s+)" + re.escape(port) + r"(\s+)"
+            search_regex = r"(^[a-z]+)(.*)(\s+)" + re.escape(port) + r"(\s+)"
             if re.match(search_regex, line):
-                return line.split("\t", 1)[0]
+                regex_out = line.split("\t", 1)[0]
+        return regex_out
 
-def ck_socket(host, port, timeout, verbose):
+def ck_socket(host, port, timeout):
     '''
     Open and check the socket:port.
-    Output strings and exit values close enough to work
+    Output strings and exit values close enough to perhaps work
      with some bash scripts formally using "nc -x to test ports."
+    Note: Returns shell true "0" if connect was successful, not "1".
     '''
     with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
         sock.settimeout(float(timeout))
-        if sock.connect_ex((host, int(port))) == 0:
-            if verbose:
-                service = get_port_service(port + "/tcp")
-                print "Connection to " + host + " " + port \
-                + " port [tcp/" + service + "] succeeded!"
-            sys.exit(0)
-        else:
-            if verbose:
-                print SCRIPT_NAME + ": connect to " + host \
-                + " port " + port + " (tcp) failed: "
-            sys.exit(1)
+        return sock.connect_ex((host, int(port)))
+
+def verbose_output(sock_status, host, port):
+    ''' Output psuedo nc -zv strings '''
+    if sock_status == 0:
+        service = get_port_service(port + "/tcp")
+        print "Connection to " + host + " " + port \
+         + " port [tcp/" + service + "] succeeded!"
+    else:
+        print SCRIPT_NAME + ": connect to " + host \
+         + " port " + port + " (tcp) failed: "
 
 def main():
     verbose, timeout, params = parse_input()
     if len(params) == 2:
         host, port = params
-        ck_socket(host, port, timeout, verbose)
+        sock_status = ck_socket(host, port, timeout)
     else:
         usage()
+    if verbose:
+        verbose_output(sock_status, host, port)
+    if sock_status != 0:
+        sys.exit(1)
 
 if __name__ == '__main__':
     sys.exit(main())
